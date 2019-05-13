c7fd09ea1c849743e91709f3b2aceff71bce6e4d
dave90dave <dave.hulst@gmail.com>
2019-05-13 13:58:05 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,9 +1,9 @@
-try: nummer1 = float(input("What is the first number?\n"))
+try: number1 = float(input("What is the first number?\n"))
 except(ValueError):
     print("This field is meant for numbers only\n")
     exit(0)
 
-try: nummer2 = float(input("What is the second number?\n"))
+try: number2 = float(input("What is the second number?\n"))
 except(ValueError):
     print("This field is meant for numbers only\n")
     exit(0)
@@ -13,12 +13,12 @@ action = input("What action do you want?\n")
 
 print("The answer is: ")
 if action is "*":
-    print(nummer1 * nummer2)
+    print(number1 * number2)
 elif action is "-":
-    print(nummer1 - nummer2)
+    print(number1 - number2)
 elif action is "+":
-    print(nummer1 + nummer2)
+    print(number1 + number2)
 elif action is "/":
-    print(nummer1 / nummer2)
+    print(number1 / number2)
 else:
     print("This is not a valid option.")
